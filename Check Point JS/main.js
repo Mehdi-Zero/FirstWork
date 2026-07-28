154fa6cb0d1fc3f228b8f8ac8d4494fa6e860730
Mehdi-Zero <samehdi456@gmail.com>
2026-07-28 18:49:33 +0100

    Add files via upload

--- Check Point JS/main.js
+++ Check Point JS/main.js
@@ -1,24 +1,64 @@
-// I chosed the first 
-function reverseString(str) {
-  return str.split("").reverse().join("")
-}
-console.log(reverseString("hello"))
+const plusBtns = document.querySelectorAll(".fa-plus-circle")
+const minusBtns = document.querySelectorAll(".fa-minus-circle")
+const deleteBtns = document.querySelectorAll(".fa-trash-alt")
+const heartBtns = document.querySelectorAll(".fa-heart")
 
-// I chose the 2nd
-function SumArray(ListOfNumbers) {
-  return ListOfNumbers.reduce((acc, curr) => acc + curr, 0)
-}
-console.log(SumArray([1, 2, 3, 4, 5])) 
+function updateTotal() {
+    let total = 0
 
-// I chosed the first
-function factorial(n) {
-  if (n < 0) return -1
-  if (n === 0 || n === 1) return 1
-  
-  let result = 1;
-  for (let i = 2; i <= n; i++) {
-    result *= i;
-  }
-  return result
+    const cards = document.querySelectorAll(".card")
+
+    cards.forEach(card => {
+        const price = parseInt(card.querySelector(".unit-price").textContent)
+        const quantity = parseInt(card.querySelector(".quantity").textContent)
+
+        total += price * quantity
+    })
+
+    document.querySelector(".total").textContent = total + " $"
 }
-console.log(factorial(5))
+
+plusBtns.forEach(btn => {
+    btn.addEventListener("click", function () {
+
+        const quantity = this.nextElementSibling
+
+        quantity.textContent = parseInt(quantity.textContent) + 1
+
+        updateTotal();
+    })
+})
+
+minusBtns.forEach(btn => {
+    btn.addEventListener("click", function () {
+
+        const quantity = this.previousElementSibling
+
+        if (parseInt(quantity.textContent) > 0) {
+            quantity.textContent = parseInt(quantity.textContent) - 1
+
+            updateTotal()
+        }
+
+    })
+})
+
+deleteBtns.forEach(btn => {
+    btn.addEventListener("click", function () {
+
+        this.closest(".card-body").remove()
+
+        updateTotal()
+
+    })
+})
+
+heartBtns.forEach(btn => {
+    btn.addEventListener("click", function () {
+
+        this.classList.toggle("text-danger")
+
+    })
+})
+
+updateTotal()
